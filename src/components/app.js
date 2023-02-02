10904e029e21d7f037b380bf01609b1e094bec5f
Walker563 <walkerml563@gmail.com>
2023-02-02 11:13:41 +0000

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+
+export default function App(){
+
+
+    return(
+
+            <div> This is a simple stateful and server-side rendered React App</div>
+
+
+
+    )
+
+
+
+
+
+
+}
---
+++ src/server/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import ReactDOMserver from "react-dom/server"
+import App from "../components/app"
+const server = express()
+
+server.use(express.static('dist'))
+
+server.get("/",(req,res)=>{
+
+    const initialMarkup = ReactDOMserver.renderToString(<App/>)
+
+    const returnString = 
+
+   // console.log("Return String ", returnString)
+
+    res.send(`<html>
+    <head>
+    <title>Sample React Application</title>
+    </head>
+    <body>
+        <div id="app" >{initialMarkup}</div>
+        <script src="/main.js"></script>
+    </body>
+ </html>` )       
+ })
+
+server.listen(4242,()=>{console.log("Listening on port 4242")
+
+
+
+})
